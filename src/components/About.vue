dd464118e605bea90c28a3ad358340b4b38c562f
Jesus Oroño <oroxo1989@gmail.com>
2024-01-14 22:11:06 -0400

    changed company description

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,11 +1,26 @@
 <template>
 	<main class="py-10">
-		<h1 class="text-center pb-6 text-3xl md:text-4xl lg:text-5xl font-semibold text-[#1521f7]">Nosotros</h1>
-		<p class="text-gray-600 text-center lg:px-24">UP-LINK de Venezuela</p>
+		<h1 class="text-center pb-6 text-3xl md:text-4xl lg:text-5xl font-semibold text-[#1521f7]">Quienes Somos</h1>
+		<p class="text-gray-600 text-center lg:px-24">UP LINK DE VENEZUELA, C.A</p>
 		<section class="mt-10 md:flex md:gap-8 md:items-start xl:items-start">
 			<img class="md:mx-auto mb-5 md:w-3/12 lg:w-2/12" src="/about.png" width="150" />
 			<p class="xl:px-6 lg:text-lg text-gray-600 xl:w-9/12 leading-thight">
-				<span class="text-indigo-700 font-medium">UP-LINK</span> es una empresa que ofrece Internet de alta velocidad en la zona norte de Anzoátegui. La empresa avanza con la expansión de su red de Internet Fibra Óptica de banda ancha simetrica con tecnología GPON en Piritu y Puerto Piritu. Desde hace 2 años, <span class="text-indigo-700 font-medium">UP-LINK</span> incursionó en el mercado oriental ofreciendo internet de alta velocidad, cubriendo la necesidad de contar con internet estable en la zona. <span class="text-indigo-700 font-medium">UP-LINK</span> busca posicionarse como la empresa #1 con los mejores estándares de calidad. <span class="text-indigo-700 font-medium">UP-LINK</span> ofrece distintos planes para conectarse a la red, ellos incluyen Residencial, Condominios, Empresarial y Corporativo. La velocidad de conexión varía de acuerdo al plan contratado, que van desde 30Mb hasta 200 Mb.
+				Somos una empresa en el área de telecomunicaciones que se especializa en ofrecer servicios de
+				transmisión satelital en toda Venezuela y en el exterior, fundada en el año 2006, desarrollándose como
+				uno de los mejores proyectos de telecomunicaciones en el país de forma eficiente, respondiendo a las
+				necesidades de los clientes a nivel nacional.
+				Ahora bien, como estrategia de crecimiento en las telecomunicaciones y con más de 17 años de
+				trayectoria, buscando siempre la innovación y así atender las necesidades cambiantes del mercado, la
+				empresa avanza en la expansión mediante la prestación de servicio de Internet vía Fibra Óptica con
+				tecnología GPON desarrollada especialmente para brindar una conexión estable y veloz a los comercios
+				u hogares del país.
+				Desde hace 2 años, <span class="text-indigo-700 font-medium">UP LINK DE VENEZUELA, C.A</span>, incursionó en la Zona Oriental del país
+				específicamente en la parte Norte del estado Anzoátegui, contando con los mejores equipos y así
+				satisfacer las necesidades de conexión, comunicación y acceso a Internet, así nos consolidamos como la
+				más efectiva alternativa del mercado actual.
+				Además, contamos con un equipo de talento humano altamente calificado, con una infraestructura y
+				tecnología de punta, dando soluciones rápidas de manera eficiente; de igual forma operamos con los
+				mejores proveedores nacionales de internet, para ofrecer planes que se adaptan a cada necesidad.
 			</p>
 		</section>
 	</main>
