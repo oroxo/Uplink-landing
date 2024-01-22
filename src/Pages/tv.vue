ad18ab1ab4bcd42bdf99f1b74bb0779ceddb5226
Jesus Oroño <oroxo1989@gmail.com>
2024-01-22 00:54:02 -0400

    sketch channels grid

--- src/Pages/tv.vue
+++ src/Pages/tv.vue
@@ -1,11 +1,12 @@
 <template>
   <section id="pagos" class="bg--gray-100">
-        <CS />
+        <TV />
     </section>
 </template>
 
 <script setup>
-import CS from '@/components/CommingSoon.vue'
+// import CS from '@/components/CommingSoon.vue'
+import TV from '@/components/TVChannels.vue'
 
 </script>
 
---
+++ src/components/TVChannels.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="relative mt-20 py-10">
+        <div class="text-center">
+            <h1>¡Mas de 50 canales!</h1>
+        </div>
+        <div class="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6">
+            <div class="flex justify-center items-center">
+                <img alt="gallery" class="block object-cover object-center rounded-lg hover:bg-sky-300" style="max-width: 90px;" src="/Canales/Globovision.webp" />
+            </div>
+            <div class="flex justify-center items-center">
+                <img alt="gallery" class="block rounded-lg object-center" style="max-width: 90px;" src="/Canales/TVes.webp" />
+            </div>
+            <div class="flex justify-center items-center">
+                <img alt="gallery" class="block rounded-lg object-center" style="max-width: 90px;" src="/Canales/Venevisión.svg" />
+            </div>
+            <div class="flex justify-center items-center">
+                <img alt="gallery" class="block rounded-lg object-center" style="max-width: 90px;" src="/Canales/VTV.webp" />
+            </div>
+            <div class="flex justify-center items-center">
+                <img alt="gallery" class="block object-cover object-center" style="max-width: 90px;" src="/Canales/Televen.webp" />
+            </div>
+            <div class="flex justify-center items-center">
+                <img alt="gallery" class="block rounded-lg object-center" style="max-width: 90px;" src="/Canales/CanalI.png" />
+            </div>
+            <div class="flex justify-center items-center">
+                <img alt="gallery" class="block rounded-lg object-center" style="max-width: 90px;" src="/Canales/Meridiano.webp" />
+            </div>
+            <div class="flex justify-center items-center">
+                <img alt="gallery" class="block rounded-lg object-center" style="max-width: 90px;" src="/Canales/ValeTV.webp" />
+            </div>
+            <div class="flex justify-center items-center">
+                <img alt="gallery" class="block object-cover object-center" style="max-width: 90px;" src="/Canales/VepacoTV.png" />
+            </div>
+            <div class="flex justify-center items-center">
+                <img alt="gallery" class="block rounded-lg object-center" style="max-width: 90px;" src="/Canales/TVFANB.jpeg" />
+            </div>
+            <div class="flex justify-center items-center">
+                <img alt="gallery" class="block rounded-lg object-center" style="max-width: 90px;" src="/Canales/ANTVLogo.webp" />
+            </div>
+            <div class="flex justify-center items-center">
+                <img alt="gallery" class="block rounded-lg object-center" style="max-width: 90px;" src="/Canales/TVFamilia.png" />
+            </div>
+            <div class="flex justify-center items-center">
+                <img alt="gallery" class="block object-cover object-center" style="max-width: 90px;" src="/Canales/Vivetv.png" />
+            </div>
+            <div class="flex justify-center items-center">
+                <img alt="gallery" class="block rounded-lg object-center" style="max-width: 90px;" src="/Canales/Concienciatv.webp" />
+            </div>
+            <div class="flex justify-center items-center">
+                <img alt="gallery" class="block rounded-lg object-center" style="max-width: 90px;" src="/Canales/tlt.png" />
+            </div>
+            <div class="flex justify-center items-center">
+                <img alt="gallery" class="block rounded-lg object-center" style="max-width: 90px;" src="/Canales/Telesur.webp" />
+            </div>
+            
+
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
