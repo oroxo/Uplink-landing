6059778ae064611c07a96d019f0749cd3676cbe1
Jesus Oroño <oroxo1989@gmail.com>
2024-01-22 23:38:21 -0400

    limit max heigth

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -12,7 +12,7 @@
         class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
         aria-label="Slide 3"></button>
     </div>
-    <div class="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
+    <div class="relative max-h-screen w-full overflow-hidden after:clear-both after:block after:content-['']">
       <div
         class="relative float-left -mr-[100%] hidden w-full !transform-none bg-cover bg-center bg-no-repeat opacity-0 transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none"
         data-te-carousel-fade data-te-carousel-item data-te-carousel-active>
